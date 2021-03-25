d118a7f72469e0baf4ffcf34ced1c4866a155d43
alumne <alumne@alumnepc>
2021-03-25 10:42:57 +0100

    primer commit de l'examen

---
+++ Accions/src/com/alumne/accions/Accions.java
@@ -0,0 +1,63 @@
+package com.alumne.accions;
+
+public class Accions {
+
+	public int registrarPreu(double preu, String ticker) {
+
+		// Si fora real guardaria a la base de dades el nom de l'empresa i el valor de
+		// l'acció
+		// però com es tracta d'una prova només els mostra per pantalla
+
+		String tickersValids = "AAPL AMZN FB GOOGL";
+
+		int comparacio = tickersValids.indexOf(ticker);
+		if (comparacio == -1) {
+
+			System.out.println("El ticker no és vàlid");
+
+			return -1;
+		}
+
+		if (preu < 0.0) {
+
+			System.out.println("El preu és menor que el valor mínim permés");
+
+			return -2;
+
+		}
+
+		if (preu > 500.0) {
+
+			System.out.println("El preu és superior al valor màxim permés");
+			return -3;
+		}
+
+		System.out.println("Valors registrats, ticker: " + ticker + ", preu: " + preu);
+		return 0;
+	}
+
+	public double preuMax() {
+		// Un programa real es connectaria a una base de dades per a mirar el preu més
+		// alt
+		// com és per a una prova ràpida mostra un valor predeterminat
+		return 455.0;
+	}
+
+	public String tickerMax() {
+		// Un programa real es connectaria a una base de dades per a mirar l'empresa amb
+		// el preu més alt
+		// com ús per a una prova ràpida mostra un valor predeterminat
+		return "AAPL";
+	}
+
+	public double preuMin() {
+		// Igual per als valors mínims
+		return 150.0;
+	}
+
+	public String tickerMin() {
+		// Igual per als valors mínims
+		return "FB";
+	}
+
+}
